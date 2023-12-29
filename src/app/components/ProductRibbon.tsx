6ba81f230c4aa5f32f052afb055ed836038adb96
Adrian Wisernig <hello@yoadrian.co>
2023-12-28 19:39:38 -0800

    Better Product Ribbon

--- src/app/components/ProductRibbon.tsx
+++ src/app/components/ProductRibbon.tsx
@@ -4,7 +4,7 @@ export default function ProductRibbon () {
   return (
     <div className="pt-5 pb-10 bg-neutral-100 drop-shadow-xl">
       <h1 className="px-5 text-3xl font-thin mb-5 font-mono border-b text-slate-800">Products We Use</h1>
-      <div className="flex flex-col md:flex-row gap-2 flex-wrap">
+      <div className="grid grid-flow-row sm:grid-cols-3 md:grid-flow-col gap-3 gap-y-5">
         <ProductLogo url="/images/vendors/Victron.webp" alt="Victron Energy" />
         <ProductLogo url="/images/vendors/B&G.webp" alt="B&G Electronics" />
         <ProductLogo url="/images/vendors/Wakespeed.webp" alt="Wakespeed" />
